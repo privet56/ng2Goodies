68e16a55a2b5ddc63f6dad1870fdf67bd09c4dfd
privet56@hotmail.com <privet56@hotmail.com>
2016-11-30 10:15:16 +0100

    better persistent router

--- PersistentRouterOutlet.ts
+++ PersistentRouterOutlet.ts
@@ -60,6 +60,9 @@ class RefCache {
 })
 export class PersistentRouterOutlet extends RouterOutlet
 {
+  public static __LASTRESOLVEDURL : string = "__resolvedurl";
+  public static __MAX2LOG : number  = 33;  
+  protected LOGGED : number         = 0;
   private cache = [];
   private location_: ViewContainerRef;
 
@@ -68,6 +71,12 @@ export class PersistentRouterOutlet extends RouterOutlet
   {
       super(parentOutletMap, location, resolver, name);
       this.location_ = location;
+      /*
+      this.activateEvents.subscribe((activatedInstance) =>
+      {
+       this.activatedInstance = activatedInstance;
+      });
+      */
   }
   activate( activatedRoute: ActivatedRoute,
             resolver: ComponentFactoryResolver,
@@ -75,60 +84,92 @@ export class PersistentRouterOutlet extends RouterOutlet
             providers: ResolvedReflectiveProvider[],
             outletMap: RouterOutletMap): void
   {
-    //activatedRoute.url is a BehaviorSubject
-    let url: string = activatedRoute.url._value[0].path;
-      let oldActivated:any = this.getActivated();
-      let activated:any = this.cache[url];
-      if(!activated)
-      {
-        super.activate(activatedRoute, resolver, injector, providers, outletMap);
-		this.cache[url] = this.getActivated();
-		this.log("router.activate FIRST: "+url);
-		this.log(this.getActivated().changeDetectorRef);
-      }
-      else
-      {
-        if(oldActivated)
+    //activatedRoute.url is a BehaviorSubject    
+    activatedRoute.url.subscribe(res =>
+    {
+        if (res != null && res.length > 0)
         {
-            let i:number = this.location_.indexOf(oldActivated.changeDetectorRef._view.ref);
-            this.log("router.DEACTIVATED("+i+"): "+oldActivated.changeDetectorRef._view.ref);
-            this.location_.detach(i);
+            let url: string = res[0].path;
+            activatedRoute[PersistentRouterOutlet.__LASTRESOLVEDURL] = url;
+
+            if(this.getActivatedRoute() && (url === this.getActivatedRoute()[PersistentRouterOutlet.__LASTRESOLVEDURL]))
+            {
+                this.log("no change in url '"+this.getActivatedRoute()[PersistentRouterOutlet.__LASTRESOLVEDURL]+"'");
+                return;
+            }
+
+            let oldActivated:any = this.getActivated();
+            let newActivated:any = this.cache[url];
+
+            if(!newActivated)
+            {   //let super handle it and cache result
+                super.activate(activatedRoute, resolver, injector, providers, outletMap);
+                this.cache[url] = this.getActivated();
+                this.log("FIRST: '"+url+"' changeDet:"+this.getActivated().changeDetectorRef);
+            }
+            else
+            {   //detach current and attach from cache
+                if(oldActivated)
+                {
+                    let i:number = this.location_.indexOf(oldActivated.changeDetectorRef._view.ref);
+                    this.log("DEACTIVATED("+i+"): changeDetViewRef:"+oldActivated.changeDetectorRef._view.ref);
+                    this.location_.detach(i);
+                }
+                this.outletMap = outletMap;
+                this.setActivatedRoute(activatedRoute);
+                this.setActivated(newActivated);
+                this.log("CACHE: "+url);
+                this.location_.insert(newActivated.changeDetectorRef._view.ref);
+                //activated.changeDetectorRef.detectChanges();  //do not call, as future is already resolved
+                this.activateEvents.emit(this.getActivated().instance);
+            }
         }
-        this.outletMap = outletMap;
-        this._activatedRoute = activatedRoute;
-        this.setActivated(activated);
-        this.log("router.activate CACHE: "+url);
-        this.location_.insert(activated.changeDetectorRef._view.ref);
-        //activated.changeDetectorRef.detectChanges();
-        this.activateEvents.emit(this.getActivated().instance);
-      }
+        else
+        {
+            this.log("ERR: !url res:"+res);
+        }
+    });
   }
   deactivate(): void
   {
-    //don't call super
+    //don't call super, because super destroy()'s
 
     if (this.getActivated())
     {
         var c = this.component;
         let i:number = this.location_.indexOf(this.getActivated().changeDetectorRef._view.ref);
-        this.log("router.DEACTIVATED("+i+"): "+this.getActivated().changeDetectorRef._view.ref);
+        this.log("deactivate()DEACTIVATED("+i+"): changeDetViewRef:"+this.getActivated().changeDetectorRef._view.ref);
         this.location_.detach(i);
         //this.activated.destroy();     //this is what supper does
         this.setActivated(null);
         this.deactivateEvents.emit(c);
     }
   }
-  getActivated()
+
+  //getter/setter for private members of super
+  protected getActivated()
   {
       return this.activated;
   }
-  setActivated(activated:any) : void
+  protected setActivated(activated:any) : void
   {
       this.activated = activated;
   }
-  
-  log(s:any) : void
+  protected getActivatedRoute()
   {
-      console.log(s);
+      return this._activatedRoute;
+  }
+  protected setActivatedRoute(activatedRoute:any) : void
+  {
+      this._activatedRoute = activatedRoute;
+  }
+  
+  public log(s:string, o?:any) : void
+  {
+      if(this.LOGGED++ > PersistentRouterOutlet.__MAX2LOG)
+      {
+          return;   //don't log too much, but do it at the beginning (and so 'force' to look for the official solution)
+      }
+      console.log(""+this.LOGGED+": router.activate:"+s);
   }
 }
