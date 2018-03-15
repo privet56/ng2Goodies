65a44d2384dcd97ff5f354988bbc064723685200
privet56@hotmail.com <privet56@hotmail.com>
2018-03-15 13:34:55 +0100

    nodeJS & MSVSCode development environment

---
+++ nodejsDevEnv/startNgProject.bat
@@ -0,0 +1,7 @@
+set "PATH=%~dp0..\devenv\git;%~dp0..\devenv\git\bin;%PATH%"
+set "PATH=%~dp0..\devenv\nodejs;%~dp0..\devenv\nodejs\node_modules\npm;%PATH%"
+
+set APPDATA=%~dp0../devenv/MsVSCode
+set USERPROFILE=%~dp0../devenv/MsVSCode
+
+start "" %~dp0../devenv/MsVSCode/Code.exe %~dp0
