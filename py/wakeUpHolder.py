3e111ef259ea7dead53bf604ca1bf53a10c6dfb5
privet <privet56@hotmail.com>
2019-06-11 15:36:46 +0200

    wakeUpHolder.py with win32 api

--- py/wakeUpHolder.py
+++ py/wakeUpHolder.py
@@ -6,6 +6,8 @@ import argparse
 
 import pyautogui    # pip install pyautogui
 
+import win32api
+
 #Issue:
 # Command "python setup.py egg_info" failed with error code 1 in c:\temp\pip-install-lamdpy\pygetwindow\
 #Solution:
@@ -20,15 +22,48 @@ class WakeUpHolder:
     def doWakeUpHolder(self, sleepInterval):
         x, y = pyautogui.position()
         left = True
+        win32Api = True
+        loopDone = 0
 
         try:
             while True:
                 time.sleep(sleepInterval)
 
+                positionStr = 'x:' + str(x).rjust(4) + ' y:' + str(y).rjust(4) + " loopDone:"+str(loopDone).zfill(4) + "("+str(loopDone*sleepInterval).zfill(4)+"sec), win32Api:"+str(win32Api).rjust(5)+" "
+
                 if (x != pyautogui.position().x) or (y != pyautogui.position().y):
-                    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4) + ' ... manually moved... -> nothing to do!'
+                    positionStr += ' manually moved: -> nothing to do!'
                 else:
-                    positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4) + ' ... mouse not moved... -> move it!'
+                    positionStr += ' mouse not moved -> move mouse!'
+                    if win32Api:
+                        newPos =(x + (10 if left else -10), y)
+                        win32api.SetCursorPos(newPos)
+                    else:
+                        pyautogui.moveRel(10 if left else -10, 0, duration=0.25)
+
+                WakeUpHolder.log(positionStr)
+                x, y = pyautogui.position()
+                left = not left
+                win32Api = not win32Api
+                loopDone = loopDone + 1
+
+        except KeyboardInterrupt:
+            WakeUpHolder.log('\nDone.')
+
+    def doWakeUpHolderWithAutoGui(self, sleepInterval):
+        x, y = pyautogui.position()
+        left = True
+
+        try:
+            while True:
+                time.sleep(sleepInterval)
+
+                positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+
+                if (x != pyautogui.position().x) or (y != pyautogui.position().y):
+                    positionStr += ' ... manually moved... -> nothing to do!'
+                else:
+                    positionStr += ' ... mouse not moved... -> move it!'
                     pyautogui.moveRel(10 if left else -10, 0, duration=0.25)
 
                 WakeUpHolder.log(positionStr)
@@ -38,10 +73,36 @@ class WakeUpHolder:
         except KeyboardInterrupt:
             WakeUpHolder.log('\nDone.')
 
+    def doWakeUpHolderWithWinApi(self, sleepInterval):
+        x, y = win32api.GetCursorPos()
+        left = True
+
+        try:
+            while True:
+                time.sleep(sleepInterval)
+
+                positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+
+                if (x != win32api.GetCursorPos()[0]) or (y != win32api.GetCursorPos()[1]):
+                    positionStr += ' ... manually moved... -> nothing to do!'
+                else:
+                    positionStr += ' ... mouse not moved... -> move it!'
+                    newPos =(x + (10 if left else -10), y)
+                    win32api.SetCursorPos(newPos)
+
+                WakeUpHolder.log(positionStr)
+                x, y = win32api.GetCursorPos()
+                left = not left
+
+        except KeyboardInterrupt:
+            WakeUpHolder.log('\nDone.')
+
+
     @staticmethod
     def log(s):
         print(s)
 
+# call: python ./wakeUpHolder.py --sleep 3
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--sleep' , type=int, default=30,  help='sleep interval in seconds')
