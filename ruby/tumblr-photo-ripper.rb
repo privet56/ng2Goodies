afb31048baa278315b3fcdc8dae5a0d2a823a765
privet <privet56@hotmail.com>
2020-06-09 15:36:01 +0200

    tumblr ripper in ruby, for both API versions

---
+++ ruby/tumblr-photo-ripper.rb
@@ -0,0 +1,70 @@
+# Usage:
+#   [sudo] gem install mechanize
+#   ruby tumblr-photo-ripper.rb
+
+require 'rubygems'
+require 'mechanize'
+
+# Your Tumblr subdomain, e.g. "jamiew" for "jamiew.tumblr.com"
+site = "pinayfarang"
+
+
+FileUtils.mkdir_p(site)
+
+concurrency = 8
+num = 50
+start = 0
+
+def handleimgurls(image_urls, concurrency, site)
+  image_urls.each_slice(concurrency).each do |group|
+    threads = []
+    group.each do |url|
+      threads << Thread.new {
+        # puts "Saving photo #{url}"
+        begin
+          file = Mechanize.new.get(url)
+          filename = File.basename(file.uri.to_s.split('?')[0])
+          file.save_as("#{site}/#{filename}")
+		  #puts "Saving photo #{url} > #{site}/#{filename}"
+        rescue Mechanize::ResponseCodeError
+          puts "Error getting file, #{$!}"
+        end
+      }
+    end
+    threads.each{|t| t.join }
+  end
+end
+
+loop do
+  puts "start=#{start}"
+
+  url = "http://#{site}.tumblr.com/api/read?type=photo&num=#{num}&start=#{start}"
+  page = Mechanize.new.get(url)
+  doc = Nokogiri::XML.parse(page.body)
+
+  images = (doc/'post photo-url').select{|x| x if x['max-width'].to_i == 1280 }
+  image_urls = images.map {|x| x.content }
+  handleimgurls(image_urls, concurrency, site)
+
+  didVersion2 = false
+  
+  #version 2
+  if images.count == 0
+	image_urls = page.body.scan(/&lt;img src=\"(.*?)\"/).flatten
+	handleimgurls(image_urls, concurrency, site)
+	num = image_urls.length()
+	images = image_urls
+	didVersion2 = true
+  end
+  
+  puts "#{images.count} images found (num=#{num})"
+  
+  if images.count < num
+    puts "our work here is done. version2: #{didVersion2}"
+    break
+  else
+    start += num
+	puts "do next page. start: #{start}  version2: #{didVersion2}"
+  end
+
+end
