01142e5650442e442e8c83dc2b119bc0c51fc114
privet <privet56@hotmail.com>
2020-06-29 17:21:30 +0200

    Repo Base Class with DB Service functions, like exists, findAll, findAllIds, deleteAll

--- sql/ServiceRepository.java
+++ sql/ServiceRepository.java
@@ -24,4 +24,27 @@ public class ServiceRepository {
         final TypedQuery<Long> tq = entityManager.createQuery(cq).setMaxResults(1); // LIMIT = 1
         return tq.getSingleResult() > 0;
     }
+    public <E extends BaseEntity> List<E> findAll(final Class<E> entityClass) {
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<E> cq = cb.createQuery(entityClass);
+        Root<E> rootEntry = cq.from(entityClass);
+        CriteriaQuery<E> all = cq.select(rootEntry);
+        TypedQuery<E> allQuery = entityManager.createQuery(all);
+        return allQuery.getResultList();
+    }
+
+    public <E extends BaseEntity> List<String> findAllIds(final Class<E> entityClass) {
+        final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        final CriteriaQuery<String> cq = cb.createQuery(String.class);
+        final Root<E> from = cq.from(entityClass);
+        cq.select(from.get(BaseEntity.ID));
+        final TypedQuery<String> tq = entityManager.createQuery(cq);
+        return tq.getResultList();
+    }
+    public <E extends BaseEntity> int deleteAll(final Class<E> entityClass) {
+        final CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        final CriteriaDelete<E> cq = cb.createCriteriaDelete(entityClass);
+        int affectedCount = entityManager.createQuery(cq).executeUpdate();
+        return affectedCount;
+    }
 }
