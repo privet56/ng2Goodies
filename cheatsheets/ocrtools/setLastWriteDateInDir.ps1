d578fce626eaf916ae84733d6c34ae61921fabfb
privet <privet56@hotmail.com>
2020-07-29 13:52:03 +0200

    setLastWriteDateInDir.ps1

--- cheatsheets/ocrtools/setLastWriteDateInDir.ps1
+++ cheatsheets/ocrtools/setLastWriteDateInDir.ps1
@@ -5,11 +5,11 @@ param ([string]$dir)    # define & get command line param
 $fileCount = 0
 
 Get-ChildItem -Path $dir -File | 
-Foreach-Object {
-    $file = Get-Item $_.FullName
-    $file.LastWriteTime = (Get-Date)
-    Write-Host "LastWriteTime set for ", $_.FullName
-    $fileCount++
-}
+    Foreach-Object {
+        $file = Get-Item $_.FullName
+        $file.LastWriteTime = (Get-Date)
+        Write-Host "LastWriteTime set for ", $_.FullName
+        $fileCount++
+    }
 
 Write-Host "Finished setting LastWriteTime for ", $fileCount, " files."
