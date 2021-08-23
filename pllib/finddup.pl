f49bc0b972808c3323c0fe284cadc6ee4651bc86
privet <privet56@hotmail.com>
2021-08-23 13:21:00 +0200

    find duplicated files by checking file sizes, in perl

--- pllib/finddup.pl
+++ pllib/finddup.pl
@@ -1,16 +1,26 @@
 #!/usr/bin/perl -w
 
-# find duplicated files in a directory by only checking the file Sizes (//TODO: checking/hash content)
-# call: ./finddup.pl /home/h/Downloads/tmp > /home/h/Downloads/tmp/sameSizes.log
+# find duplicated files in a directory by only checking the fileSize or checkSum of the file
+# call: ./finddup.pl /home/h/Downloads/tmp 0 > /home/h/Downloads/tmp/sameSizes.log  2>&1
+
+# 1. param: dir to check
+# 2. param: 0 or 1 = useCheckSum off/on
 
 use strict;
 use warnings;
 
-my $dir = $ARGV[0];
-my %fileSize2Name = ();
-my $sameSizes = 0;
+use 5.010; # so i can use 'state'
 
-opendir(DH, $dir);
+use Digest::MD5 qw(md5_hex);
+use IO::File;
+
+my $dir = $ARGV[0];
+my $useCheckSum = $#ARGV > 0 ? ($ARGV[1] == '1') : 0;
+my %fileKey2Name = ();
+my $sameKeys = 0;
+my $fileKey2Names = 0;
+
+opendir(DH, $dir) or die "!open $dir";
 my @files = readdir(DH);
 closedir(DH);
 
@@ -23,16 +33,64 @@ foreach(@files)
     if (-f $absFn)                                      # = is a file
     {
         my $fileSize = -s $absFn;                       # = getFileSize, similar to stat
-        if (exists($fileSize2Name{$fileSize})) {
-            print "$fn <sameSize> $fileSize2Name{$fileSize}\n";
-            $sameSizes = $sameSizes + 1;
+        my $key = $fileSize;
+        $fileKey2Names = (keys %fileKey2Name);
+        if ($useCheckSum)
+        {
+            my $checkSum = getCheckSum($absFn);
+            $key = $checkSum;
+        }
+        
+        if ($useCheckSum and (($fileKey2Names % 10) == 0))
+        {
+            _log(".");
+        }
+        
+        if (exists($fileKey2Name{$key}))
+        {
+            _log("$fn <sameKey $key> $fileKey2Name{$key}");
+            $sameKeys++;
         }
         else
         {
-            $fileSize2Name{$fileSize} = $fn;
+            $fileKey2Name{$key} = $fn;
         }
     }
 }
 
-my $fileSize2Names = (keys %fileSize2Name);
-print "FINISH. checked $fileSize2Names files. sameSizes = $sameSizes\n";
+$fileKey2Names = (keys %fileKey2Name);
+_log("FINISH. checked $fileKey2Names files. sameSizes = $sameKeys");
+
+sub getCheckSum
+{
+    my $absFn = $_[0];
+    open (my $FH, $absFn) or die "!open $absFn";
+    binmode ($FH);
+    my $checkSum = Digest::MD5->new->addfile($FH)->hexdigest;
+    close ($FH);
+    return $checkSum;
+}
+
+sub _log
+{
+    my $s = $_[0];
+    my $slen = length($s);
+    
+    state $startWithNewLine = 0;
+    
+    my $prefix  = ($startWithNewLine == 1) ? "\n" : "";
+    my $postfix = ($slen < 2) ? "" : "\n";
+
+    if ($slen < 2)
+    {
+        $postfix = "";
+        $prefix  = "";
+        $startWithNewLine = 1;
+    }
+    else
+    {
+        $startWithNewLine = 0;    
+    }
+
+    print STDERR $prefix.$s.$postfix;
+}
