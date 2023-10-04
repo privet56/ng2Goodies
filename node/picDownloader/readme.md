244c063beea64a2993dc62847aa102bdd321a178
privet <privet56@hotmail.com>
2023-10-04 16:27:52 +0200

    pic-downloader as node js project

--- node/picDownloader/readme.md
+++ node/picDownloader/readme.md
@@ -1,9 +1,13 @@
-# setup commands
+# setup ts project
 npm install typescript --save-dev
 npm install @types/node --save-dev
 npx tsc --init --rootDir src --outDir build --esModuleInterop --resolveJsonModule --lib es6 --module commonjs --allowJs true --noImplicitAny true
 npm install --save-dev ts-node nodemon
 npm install --save-dev rimraf
+
+# project specific libs
+npm install axios
+npm install image-downloader
 
 # start command
 npm run start:dev
--- node/picDownloader/src/index.ts
+++ node/picDownloader/src/index.ts
@@ -1,1 +1,32 @@
-console.log('Hello world!')
+const fs = require('fs');
+const Axios = require('axios')
+
+const download = require('image-downloader');
+
+const folderName = "r7";
+
+if (!fs.existsSync("_dwnld_"+folderName)) {
+    fs.mkdirSync("_dwnld_"+folderName);
+}
+
+function downloadImage(url: string, filepath: string) {
+    return download.image({
+       url,
+       dest: filepath 
+    });
+}
+
+const min: number = 1;
+const max: number = 66;
+
+for(let index: number = min; index <= max; index++) {
+    const sindex: string = index.toString().padStart(2, "0");
+    const url = `http://dydudu.hu/kepreg/rejto/${folderName}/k${sindex}.jpg`;
+    const absFN = `${__dirname}/../_dwnld_${folderName}/k${sindex}.jpg`;
+    const asyncDownload = downloadImage(url, absFN);
+    // TODO: await asyncDownload;
+}
+
+// TODO: pdf from downloaded pic
+
+// export {}; // to allow await
