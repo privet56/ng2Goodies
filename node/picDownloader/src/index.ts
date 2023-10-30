a34a06e5275e8b96cb80c3231a50a2ab33283fbc
privet <privet56@hotmail.com>
2023-10-30 12:35:37 +0100

    pic downloader and pdf creator with async sequencial download

--- node/picDownloader/src/index.ts
+++ node/picDownloader/src/index.ts
@@ -1,26 +1,66 @@
 import * as picDLib from "./picDLib";
 
-// TODO: iterate all folders: r1 to r??
-const folderName = "r3";
-const absFNs: Array<string> = [];
+// TODO: theme: get as script params
+//const theme:string = "rejto";
+const theme:string = "rahan";
+const max = 26; // TODO: break if folder not found!
 
-picDLib.mkdir("_dwnld_"+folderName);
+const folderNamePDF: string = "hu." + theme + ".kepregeny";
 
-function downloadNextImage(index: number, folderName: string, absFNs: Array<string>) {
+// TODO: create options object instead of a lot of parameter!
+function downloadNextImage(index: number, folderName: string, absFNs: Array<string>, onEnd: (fn: string) => void) {
     const sindex: string = index.toString().padStart(2, "0");
-    const url = `http://dydudu.hu/kepreg/rejto/${folderName}/k${sindex}.jpg`;
-    const absFN = `${__dirname}/../_dwnld_${folderName}/k${sindex}.jpg`;
+    const url = `http://dydudu.hu/kepreg/${theme}/${folderName}/k${sindex}.jpg`;
+    const absFN = `${__dirname}/../_dwnld_${theme}_${folderName}/k${sindex}.jpg`;
     const asyncDownload = picDLib.downloadImage(url, absFN);
     asyncDownload.then((opts: any) => {
         absFNs.push(absFN);
-        downloadNextImage(index + 1, folderName, absFNs);
+        downloadNextImage(index + 1, folderName, absFNs, onEnd);
       })
       .catch((err: any) => {
-          console.error("error: " + err);
-          picDLib.makepdf(`${__dirname}/../_dwnld_${folderName}/${folderName}.pdf`, absFNs);
 
+          // TODO: check kind of error: 404 is expected, anything else is a real error
+
+          if (index > 1) {
+            const fn:string = `${__dirname}/../${folderNamePDF}/hu.${theme}.kepregeny.${folderName}.pdf`;
+            picDLib.makepdf(fn, absFNs);
+            console.error("ended downloading(i:"+index+", folder: "+folderName+", absFNs#: "+absFNs.length+"): url: "+ url);
+            onEnd(fn);
+          }
+          else {
+            console.error("error downloading(i:"+index+", folder: "+folderName+", absFNs#: "+absFNs.length+"): url: "+ url);
+            onEnd("");
+          }
       });
-
 }
 
-downloadNextImage(1, folderName, absFNs);
+function downloadNextImageSet(imageSetNr: number): Promise<any> {
+  const folderName = "r" + imageSetNr;
+  const absFNs: Array<string> = [];
+  picDLib.mkdir("_dwnld_" + theme + "_" + folderName);
+
+  // TODO: use async/await
+  const p: Promise<any> = new Promise((resolve: (value: any) => void, reject: (reason?: any) => void) => {
+
+    const onEnd = (fn: string) : void => {
+      resolve(fn);
+    }
+    downloadNextImage(1, folderName, absFNs, onEnd);
+  });
+  
+  return p;
+}
+
+function doDownload(imageSetNr: number) {
+  downloadNextImageSet(imageSetNr).then((opts: any) => {
+    if (imageSetNr < max) {
+      doDownload(imageSetNr + 1);
+    }
+  })
+  .catch((err: any) => {
+      console.error("error in doDownload", err);
+  });
+}
+
+picDLib.mkdir(folderNamePDF);
+doDownload(1);
--- node/picDownloader/src/picDLib.ts
+++ node/picDownloader/src/picDLib.ts
@@ -8,18 +8,19 @@ function mkdir(dirName: string): void {
     }    
 }
 
-function downloadImage(url: string, filepath: string) {
+function downloadImage(url: string, filepath: string) : Promise<any> {
     // TODO: check if target pic already downloaded
-    return download.image({
+    const p = download.image({
        url,
        dest: filepath 
     });
+    return p;
 }
 
 function makepdf(absFNPdf: string, absFNPics: Array<string>): void {
     // TODO: check if target pdf already exists.
     imgToPDF(absFNPics, imgToPDF.sizes.A4).pipe(fs.createWriteStream(absFNPdf));
-    console.log("created("+absFNPics.length+"): " + absFNPdf);
+    console.error("created("+absFNPics.length+"): " + absFNPdf);
 }
 
 module.exports = { mkdir, downloadImage, makepdf };
