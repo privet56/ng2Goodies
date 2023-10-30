447d1826580b68f65949f38099846db1af555c2f
privet <privet56@hotmail.com>
2023-10-30 14:40:41 +0100

    pic downloader and pdf creator with async sequencial download

--- node/picDownloader/src/index.ts
+++ node/picDownloader/src/index.ts
@@ -1,13 +1,13 @@
 import * as picDLib from "./picDLib";
 
 const theme:string = "rahan"; // TODO: theme: get as script params // "rejto";
-const max = 99; // TODO: break if folder not found!
+const bDoPad = false;         // TODO: rejto needs padding, rahan not!
 
 const folderNamePDF: string = "hu." + theme + ".kepregeny";
 
 // TODO: create options object instead of a lot of parameter!
-function downloadNextImage(index: number, folderName: string, absFNs: Array<string>, onEnd: (fn: string) => void) {
-    const sindex: string = index.toString().padStart(2, "0");
+function downloadNextImage(index: number, folderName: string, absFNs: Array<string>, onEnd: (fn: string|null) => void) {
+    const sindex: string = bDoPad ? index.toString().padStart(2, "0") : index.toString();
     const url = `http://dydudu.hu/kepreg/${theme}/${folderName}/k${sindex}.jpg`;
     const absFN = `${__dirname}/../_dwnld_${theme}_${folderName}/k${sindex}.jpg`;
     const asyncDownload = picDLib.downloadImage(url, absFN);
@@ -27,7 +27,7 @@ function downloadNextImage(index: number, folderName: string, absFNs: Array<stri
           }
           else {
             console.error("error downloading(i:"+index+", folder: "+folderName+", absFNs#: "+absFNs.length+"): url: "+ url);
-            onEnd("");
+            onEnd(null);
           }
       });
 }
@@ -40,7 +40,7 @@ function downloadNextImageSet(imageSetNr: number): Promise<any> {
   // TODO: use async/await
   const p: Promise<any> = new Promise((resolve: (value: any) => void, reject: (reason?: any) => void) => {
 
-    const onEnd = (fn: string) : void => { // TODO: better with 2 params, where first param is the error object (null if everything went well)
+    const onEnd = (fn: string|null) : void => { // TODO: better with 2 params, where first param is the error object (null if everything went well)
       if (fn) {
         resolve(fn);
       }
@@ -56,7 +56,7 @@ function downloadNextImageSet(imageSetNr: number): Promise<any> {
 
 function doDownloadAllmageSet(imageSetNr: number) {
   downloadNextImageSet(imageSetNr).then((opts: any) => {
-    if (imageSetNr < max) {
+    if (imageSetNr < 99) { // i don't expect more than 99 imageSets within one theme
       doDownloadAllmageSet(imageSetNr + 1);
     }
   })
