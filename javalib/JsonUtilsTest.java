3dd3e2a3f9f554e467943c877fccf3ae769a4002
privet <privet56@hotmail.com>
2024-02-15 14:34:01 +0100

    json utils in java EE with jackson

--- javalib/JsonUtilsTest.java
+++ javalib/JsonUtilsTest.java
@@ -12,6 +12,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
